3abbdd260e6fd9f65672d143f8ac8701c1182009
Ted Wallace <ted.wallace@gmail.com>
2015-07-25 08:52:43 -0700

    Initial work on functions.  Am able to set the matrix and retrieve it, can also get the unset inverse.  not able to set inverse yet.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file creates two functions makeCacheMatrix and cacheSolve
+## which enables you to create a special form of matrix with 
+## makeCacheMatrix and cache (and return) the inverse of the
+## matrix with cacheSolve.
 
-## Write a short comment describing this function
+## takes a square matrix as input and creates a special matrix with a cacheable inverse.
+## this function assumes a square/invertable matrix is provided.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(solve) inv <<- solve
+    getinv <- function() inv
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## checks for a cached version of the inverse of the supplied special matrix
+## and returns that value if present.  if not, computes the inverse and
+## caches it for future reference.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+    inv <- x$getinv()
+    if (!is.null(inv)) {
+        message("getting cached data...")
+        return(inv)
+    }
+    mat <- x$get
+    inv <- solve(mat)
+    x$setinv(inv)
+    inv
 }
